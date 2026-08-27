e10e300c328e90c8d6d9436b6eac52d2080078ba
Alchemist <per5cm@proton.me>
2026-08-27 11:18:35 +0200

    bla

--- GordonRamsay.Core/Library/Ingredient.cs
+++ GordonRamsay.Core/Library/Ingredient.cs
@@ -2,7 +2,7 @@ namespace GordonRamsay.Core.Library;
 
 class Ingredient
 {
-    public string IngredientName { get; init; }
+    public required string IngredientName { get; init; }
     internal int Quantity { get; init; }
     internal int Weight { get; set; }
 
